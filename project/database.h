7f444523de045e3151ef5ee08173ccff9b5ab3b3
Bo Kleynen <kleynenbo@gmail.com>
2018-11-30 22:20:34 +0100

    Use a queue to pass values to writeTask

--- project/database.h
+++ project/database.h
@@ -3,17 +3,15 @@
 
 #include <Arduino_FreeRTOS.h>
 #include <semphr.h>
+#include <queue.h>
 #include <Arduino.h>
 
 class Database {
 public:
   Database(byte head = -1, int nRecords = 0, unsigned int initialRecord = NULL);
-  unsigned int readLast();
-  unsigned int read(byte index);
   void write(unsigned int rec);
   void printAll(void);
   void printLast(void);
-  static void writeTask(void *args);
 
 private:
   // stores the index of the last written record
@@ -24,11 +22,18 @@ private:
   
   // semaphore handle
   SemaphoreHandle_t xSemaphore;
-  
+
+  // Queue handle
+  QueueHandle_t queueHandle;
+
+  static void writeTask(void *args);
+  static inline double calcTemp(unsigned int value);
   inline int physicalAddress(byte index);
   inline int headAddress();
   void incrementHead();
   void incrementNRecords();
+  unsigned int readLast();
+  unsigned int read(byte index);
 };
 
 #endif
